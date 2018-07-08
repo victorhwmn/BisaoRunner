30e26ad24e77f3875c4faa26dbae29edee6b9b89
Anderson Garrote <ander.user@gmail.com>
2018-07-07 22:39:18 -0300

    Mudança nos scripts de inimigos

---
+++ packed/Obst_Uni.gd
@@ -0,0 +1,19 @@
+extends RigidBody2D
+#Enemy Unicornio
+
+var speed=1.3
+
+func _ready():
+	add_to_group("Inimigo")
+	set_process(true)
+	
+	$sfx.play()
+	
+	pass
+
+func _process(delta):
+	pass
+func hit_by_player():
+	get_parent().remove_child(self)
+	pass
+
