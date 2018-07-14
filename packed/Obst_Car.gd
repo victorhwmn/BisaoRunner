b909f496f609ff7bd5fd4d9101421d4ebd3dc56c
andrebariani <andrebariani@gmail.com>
2018-07-13 22:24:37 -0300

    buzina adicionada, som do busao modificado

--- packed/Obst_Car.gd
+++ packed/Obst_Car.gd
@@ -1,13 +1,14 @@
 extends RigidBody2D
 #Enemy Carro
 
+var honk = 0
 var speed=1.3
 func _ready():
 	add_to_group("Inimigo")
 	set_process(true)
 	
 	$sfx.play()
-	#$honk.play()
+	$honk.play()
 	
 	pass
 
@@ -18,3 +19,10 @@ func hit_by_player():
 	get_parent().remove_child(self)
 	pass
 
+
+
+func _on_honk_finished():
+	if honk == 0:
+		$honk.play()
+		honk+=1
+	pass # replace with function body
