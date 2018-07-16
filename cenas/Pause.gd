f1818b7f8849701f6db7108502befaf5e40396d1
Anderson Garrote <ander.user@gmail.com>
2018-07-16 14:19:27 -0300

    correcao bug pause

--- cenas/Pause.gd
+++ cenas/Pause.gd
@@ -6,5 +6,6 @@ func _ready():
 
 func _input(event):
 	if(event.is_action_pressed("ui_cancel")):
-		get_tree().paused = !(get_tree().paused)
+		get_tree().paused = !(get_tree().paused);
+		get_child(0).set_visible(!get_child(0).is_visible());
 	pass
