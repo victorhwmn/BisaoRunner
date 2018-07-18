74ba29842ef65b72f1882d37958e9a3c06c58b10
andrebariani <andrebariani@gmail.com>
2018-07-18 09:10:23 -0300

    Revert "correcao merge mainGame"
    This reverts commit 42ea43f803eb47898c25198641fbf7da4b0d082d, reversing
    changes made to 20d21d9c82a27c114133c2568a0c3f2bce26f02e.

--- cenas/Pause.gd
+++ cenas/Pause.gd
@@ -6,6 +6,5 @@ func _ready():
 
 func _input(event):
 	if(event.is_action_pressed("ui_cancel")):
-		get_tree().paused = !(get_tree().paused);
-		get_child(0).set_visible(!get_child(0).is_visible());
+		get_tree().paused = !(get_tree().paused)
 	pass
