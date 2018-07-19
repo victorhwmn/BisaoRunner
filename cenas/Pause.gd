494f6f8d0481a5b55cef72b9c410a37b5e4f06b5
Anderson Garrote <ander.user@gmail.com>
2018-07-19 17:13:10 -0300

    Tela de pause adicionada

--- cenas/Pause.gd
+++ cenas/Pause.gd
@@ -6,5 +6,7 @@ func _ready():
 
 func _input(event):
 	if(event.is_action_pressed("ui_cancel")):
+		var texto = get_child(0);
+		texto.set_visible(!texto.is_visible());
 		get_tree().paused = !(get_tree().paused)
 	pass
