382e2b25329772ddd308e135ea3208b08a13c107
Anderson Garrote <ander.user@gmail.com>
2018-08-18 16:53:46 -0300

    Animação horizonte corrigida

--- packed/AnimatedSprite.gd
+++ packed/AnimatedSprite.gd
@@ -4,8 +4,11 @@ func _ready():
 	pass
 	
 func _process(delta):
+	var veloc = 2;
 	#Recebe a velocidade do player
-	var veloc = get_node("/root/mainGame/Player").velocidade/60
+	var player = get_node("/root/mainGame/Player");
+	if(player):
+		veloc = player.velocidade/50;
 	#Define como velocidae da animação
-	frames.set_animation_speed("Movimento",veloc);
+	frames.set_animation_speed("chao",veloc);
 	pass
